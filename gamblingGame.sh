92d37f7084128865be10d541d98a4b80a12b8972
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-07-15 19:37:32 +0530

    [VipulA] ADD: Gambler if won or losse 50% of the stake

--- gamblingGame.sh
+++ gamblingGame.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+# Welcome Message
 echo "Welcome to the world of Gambling."
-
+ 
 # Assign variables and add values(Money).
-stakeMoneyPerDay=100;
-betMoneyPerGame=1;
+STAKE_MONEY_PER_DAY=100;
+BET_MONEY_PER_GAME=1;
 
-# Assign another variable and inside use RANDOM to generate numbers between 0 and 1.
-gameResult=$(($RANDOM%2))
+# Added variables for adding a limit for minimum money lost and maximum money wins.
+STAKE_PERCENT=$(($((STAKE_MONEY_PER_DAY))*50/100));
+MAX_MONEY_WIN_PER_DAY=$((STAKE_MONEY_PER_DAY+STAKE_PERCENT));
+MIN_MONEY_LOST_PER_DAY=$((STAKE_MONEY_PER_DAY-STAKE_PERCENT));
+moneyEarns=$((STAKE_MONEY_PER_DAY));
 
-# Apply if condition and find if Gamble wins or lost.
-if [[ $gameResult -eq 1 ]]
-then
-	echo "Gambler Win"
-	moneyEarns=$(($stakeMoneyPerDay+1))
-else
-	echo "Gambler lost"
-	moneyEarns=$(($stakeMoneyPerDay-1))
-fi
+# Apply while condition and checks the condition mets or not
+while [[ $moneyEarns -le $MAX_MONEY_WIN_PER_DAY && $moneyEarns -ge $MIN_MONEY_LOST_PER_DAY ]]
+do
+	gameResult=$(($RANDOM%2));
 
-#End of Use Case 02
+	# Apply if condition and if Gamble wins or lost.
+	if [[ $gameResult -eq 1 ]]
+	then
+		echo "Gambler Win"
+		moneyEarns=$((moneyEarns+BET_MONEY_PER_GAME));
+	else
+		echo "Gambler lost"
+		moneyEarns=$((moneyEarns-BET_MONEY_PER_GAME));
+	fi
+done
+
+#End of Use Case 03
