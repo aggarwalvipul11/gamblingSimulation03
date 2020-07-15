c601aa587fb948507b0b9e50fd49fa5cb499dba9
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-07-15 10:52:11 +0530

    [VipulA] ADD: Gambler assign Stakes and bet.

---
+++ gamblingGame.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+# Welcome Message
+echo "Welcome to the world of Gambling."
+
+# Declare CONSTANTS variables and assign values. 
+STAKE_MONEY_PER_DAY=100;
+BETA_MONEY_PER_GAME=1;
+
+#End of Use Case 01
