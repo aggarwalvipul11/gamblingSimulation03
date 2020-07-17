eefa204bee0b9e74ca39e50b6966d8f3dd5c1baa
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-07-17 12:43:33 +0530

    [VipulA] REFACTOR: Gambler either win or lost

--- gamblingGame.sh
+++ gamblingGame.sh
@@ -4,19 +4,21 @@ echo "Welcome to the world of Gambling."
 
 # Assign variables and add values(Money).
 stakeMoneyPerDay=100;
-betMoneyPerGame=1;
 
 # Assign another variable and inside use RANDOM to generate numbers between 0 and 1.
 gameResult=$(($RANDOM%2))
 
 # Apply if condition and find if Gamble wins or lost.
-if [[ $gameResult -eq 1 ]]
-then
-	echo "Gambler Win"
-	moneyEarns=$(($stakeMoneyPerDay+1))
-else
-	echo "Gambler lost"
-	moneyEarns=$(($stakeMoneyPerDay-1))
-fi
+function findWinOrLoss() {
+	if [[ $gameResult -eq 1 ]]
+	then
+		echo "Gambler Win"
+		moneyEarns=$(($stakeMoneyPerDay+1))
+	else
+		echo "Gambler lost"
+		moneyEarns=$(($stakeMoneyPerDay-1))
+	fi
+}
 
+findWinOrLoss
 #End of Use Case 02
